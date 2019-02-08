41d6bc2c51b10bc10fb7076b877d452fe367fa14
Zhen Data <zhenyang.9261@gmail.com>
2019-02-08 08:23:45 -0500

    Added stations and tobs, need to work on session recycle

--- climate.py
+++ climate.py
@@ -84,12 +84,57 @@ def precipitation():
     all_precipitation = []
     for precipitation in result:
         precipitation_dict = {}
-        precipitation_dict["date"] = precipitation.date
-        precipitation_dict["prcp"] = precipitation.prcp
+        #precipitation_dict["date"] = precipitation.date
+        precipitation_dict[precipitation.date] = precipitation.prcp
         all_precipitation.append(precipitation_dict)
 
     return jsonify(all_precipitation)
 
+# Return a JSON list of stations from the dataset.
+@app.route("/api/v1.0/stations")
+def stations():
+    """Return a list of stations"""
+
+    result1 = session.query(Station.id, Station.name).all()
+    all_stations = []
+    for station in result1:
+        station_dict = {}
+        station_dict['ID'] = station.id
+        station_dict['Station Name'] = station.name
+        all_stations.append(station_dict)
+        
+    return jsonify(all_stations)
+
+# Return a JSON list of stations from the dataset.
+@app.route("/api/v1.0/tobs")
+def tobs():
+    """Return a list of temperature from previous year"""
+
+    # Find the last data point 
+    last_date1 = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+
+    # Convert last date to String
+    last_date1 = "%s" %(last_date1)
+
+    # Calculate the date 1 year ago
+    first_date1 = dparser.parse(last_date1, fuzzy=True) - dt.timedelta(days=365)
+
+    # Convert from datatime type to string in the format of yyyy-mm-dd
+    first_date1 = first_date1.strftime('%Y-%m-%d')
+
+    result_tobs = session.query(Measurement.tobs).\
+    filter(Measurement.date >= first_date1).\
+    filter(Measurement.date <= last_date1).all()
+    #filter(Measurement.station == most_active_stat).all()
+
+    all_tobs = []
+    for tobs in result_tobs:
+        tob_dict = {}
+        tob_dict['Temperature'] = tobs.tobs
+        
+        all_tobs.append(tob_dict)
+        
+    return jsonify(all_tobs)
 
 if __name__ == '__main__':
     app.run(debug=True)
